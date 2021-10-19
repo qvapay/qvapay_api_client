6b744df0972090289db0b272ad591d7f20daedea
Yeikel Uriarte Arteaga <uyeikel@gmail.com>
2021-10-18 22:48:17 -0500

    docs: improve documentation
    added type of exceptions to method closed #4

--- lib/src/qvapay_api.dart
+++ lib/src/qvapay_api.dart
@@ -1,3 +1,4 @@
+import 'package:qvapay_api_client/src/exception.dart';
 import 'package:qvapay_api_client/src/models/me.dart';
 import 'package:qvapay_api_client/src/models/transaction.dart';
 
@@ -9,15 +10,25 @@ abstract class QvaPayApi {
   static const String baseUrl = 'https://qvapay.com/api/app';
 
   /// Authentication on the `QvaPay` platform.
+  ///
+  /// Throws an [AuthenticateException] login error occurs.
+  ///
+  /// Throws an [ServerException] when an error occurs on the server.
   Future<String> logIn({
     required String email,
     required String password,
   });
 
   /// Logout the `QvaPay` platform.
+  ///
+  /// Throws an [ServerException] when an error occurs on the server.
   Future<void> logOut();
 
   /// Create a new user on the `QvaPay` platform.
+  ///
+  /// Throws an [RegisterException] registry error occurs.
+  ///
+  /// Throws an [ServerException] when an error occurs on the server.
   Future<void> register({
     required String name,
     required String email,
@@ -26,26 +37,35 @@ abstract class QvaPayApi {
   });
 
   /// Obtain user data when is authenticated on the `QvaPay` platform.
+  ///
+  /// Throws an [UnauthorizedException] if your are not authenticated
+  /// on the plataform.
+  ///
+  /// Throws an [ServerException] when an error occurs on the server.
   Future<Me> getUserData();
 
   /// Get the last 30 transactions from the authenticated user.
   ///
   /// In `QvaPay` everything constitutes a transaction.
-  /// You can filter the result using the parameters:
-  /// start: date_time
-  /// end: date_time
-  /// status: [paid, pending, canceled]
-  /// remote_id: string
-  /// description: string
+  /// All transactios You can filter the result using the parameters.
+  ///
+  /// Throws an [UnauthorizedException] if your are not authenticated
+  /// on the plataform.
+  ///
+  /// Throws an [ServerException] when an error occurs on the server.
   Future<List<Transaction>> getTransactions({
     DateTime start,
     DateTime end,
-    List<String> status,
     String remoteId,
     String description,
   });
 
   /// Get all the details of a specific transaction based on its `UUID`.
+  ///
+  /// Throws an [UnauthorizedException] if your are not authenticated
+  /// on the plataform.
+  ///
+  /// Throws an [ServerException] when an error occurs on the server.
   Future<Transaction?> getTransactionDetails({required String uuid});
 
   /// Authentication status.
@@ -64,7 +84,11 @@ enum OAuthStatus {
   unauthenticated,
 }
 
-/// Use to implement a custom token storage
+/// {@template oauth_memory}
+/// An interface to implement a custom token storage.
+///
+/// {@macro oauth_memory_storage}
+/// {@endtemplate}
 abstract class OAuthStorage {
   /// Save a token in storage
   Future<bool> save(String token);
@@ -76,7 +100,24 @@ abstract class OAuthStorage {
   Future<void> delete();
 }
 
-/// Save the token in memory
+/// {@template oauth_memory_storage}
+/// Save the token in memory.
+///
+/// ```dart
+/// class OAuthMemoryStorage extends OAuthStorage {
+///   String? _token;
+///   @override
+///   Future<void> delete() async => _token = null;
+///   @override
+///   Future<String?> fetch() async => _token;
+///   @override
+///   Future<bool> save(String token) async {
+///     _token = token;
+///     return true;
+///   }
+/// }
+/// ```
+/// {@endtemplate}
 class OAuthMemoryStorage extends OAuthStorage {
   String? _token;
 
